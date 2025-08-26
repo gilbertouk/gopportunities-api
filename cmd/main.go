8a31c2558b6b71c13d0aba3ebbcab70341dafbd3
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-08-26 23:03:58 +0100

    feat: implement logger functionality and initialize config in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/gilbertouk/gopportunities-api/config"
 	"github.com/gilbertouk/gopportunities-api/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
-	fmt.Println("Go Opportunities Rest API")
+	logger = config.GetLogger("main")
+	logger.Info("Go Opportunities Rest API")
+
+	// Initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
 
 	// Initialize the router
 	router.Initialize()
